42b452a0e7a16376f911acf468931b3d38bdd408
corneavlad98 <corneavlad98@yahoo.com>
2020-11-14 18:26:09 +0200

    Sent message from RPI to confirm connection again

--- Raspberry Pi/firebase_insert.py
+++ Raspberry Pi/firebase_insert.py
@@ -1,10 +1,6 @@
 from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://homeautomationapp-42e27.firebaseio.com/")
-data = {
-    'Name': 'test 1 2 3',
-    'Type': 'test & research',
-    'Data': 'ON'
-}
-result = firebase.post('/homeautomationapp-42e27/TestTable', data)
-print(result)
+
+result = firebase.post('/Raspberryi Pi','Hello from RPI3!')
+print("sent hello to database")
