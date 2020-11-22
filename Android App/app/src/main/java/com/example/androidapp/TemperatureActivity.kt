6f034cbec79c719e3d4e1a6801d51bc0f19ec8fb
corneavlad98 <48709398+corneavlad98@users.noreply.github.com>
2020-11-22 16:34:35 +0200

    Added main activity for choosing desired utilities; Main activity contains an image button to access temperature monitoring feature;

---
+++ Android App/app/src/main/java/com/example/androidapp/TemperatureActivity.kt
@@ -0,0 +1,64 @@
+package com.example.androidapp
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toolbar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+var database = FirebaseDatabase.getInstance()
+var myDbReference = database.getReference()
+
+class TemperatureActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperature)
+
+        val temperatureTextView = findViewById<TextView>(R.id.textViewTemperature)
+        val humidityTextView = findViewById<TextView>(R.id.textViewHumidity)
+
+        val valueListener = object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val humidity = snapshot.child("RaspberryPi/DHT11/Humidity/Value%").getValue();
+                val temperature = snapshot.child("RaspberryPi/DHT11/Temperature/Value*C").getValue();
+
+                if( humidity != null) {
+                    val humidityText = "Humidity: $humidity % ";
+                    humidityTextView.setText(humidityText);
+                }
+                else {
+                    val humidityText = "Error at getting humidity!";
+                    humidityTextView.setText(humidityText);
+                }
+
+                if( temperature != null) {
+                    val temperatureText = "Temperature: $temperature *C"
+                    temperatureTextView.setText(temperatureText);
+                }
+                else {
+                    val temperatureText = "Error at getting temperature!";
+                    temperatureTextView.setText(temperatureText);
+                }
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+
+        }
+        myDbReference.addValueEventListener(valueListener);
+
+        val returnButton = findViewById<Button>(R.id.returnButton)
+        returnButton.setOnClickListener(){
+            myDbReference.removeEventListener(valueListener)
+            finish();
+        }
+    }
+}
