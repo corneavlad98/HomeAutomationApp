157783f7e8756846a86914541d7282d6af141991
Vlad Cornea <corneavlad98@yahoo.com>
2021-01-02 15:03:21 +0200

    Removed color switches, added buttons for every color; Added a disco mode, that will cycle fast through colors.

--- Android App/app/src/main/java/com/example/androidapp/LEDActivity.kt
+++ Android App/app/src/main/java/com/example/androidapp/LEDActivity.kt
@@ -3,10 +3,9 @@ package com.example.androidapp
 import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.os.Handler
-import android.view.Gravity
-import android.view.View
-import android.widget.*
-import androidx.appcompat.app.AlertDialog
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
 
@@ -14,139 +13,99 @@ import com.google.firebase.database.FirebaseDatabase
 class LEDActivity : AppCompatActivity(){
     var database = FirebaseDatabase.getInstance()
     var myDbReference = database.getReference()
-    private fun resetSwitches(){
+
+    private fun resetValues(){
         myDbReference.child("RaspberryPi/LED/LED1").setValue(0)
         myDbReference.child("RaspberryPi/LED/LED2").setValue(0)
         myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
         myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
         myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_l_e_d)
-        resetSwitches()
+        resetValues()
 
         Toast.makeText(this, "Make sure that the LED script is running!", Toast.LENGTH_LONG).show();
         val ledImageButton1 = findViewById<ImageButton>(R.id.normalLed1)
         val ledImageButton2 = findViewById<ImageButton>(R.id.normalLed2)
 
-        val rgbLedRedSwitch = findViewById<Switch>(R.id.redSwitch)
-        val rgbLedGreenSwitch = findViewById<Switch>(R.id.greenSwitch)
-        val rgbLedBlueSwitch = findViewById<Switch>(R.id.blueSwitch)
+        val redButton = findViewById<Button>(R.id.redButton)
+        val greenButton = findViewById<Button>(R.id.greenButton)
+        val blueButton = findViewById<Button>(R.id.blueButton)
+        val magentaButton = findViewById<Button>(R.id.magentaButton)
+        val cyanButton = findViewById<Button>(R.id.cyanButton)
+        val yellowButton = findViewById<Button>(R.id.yellowButton)
+        val whiteButton = findViewById<Button>(R.id.whiteButton)
+        val offButton = findViewById<Button>(R.id.offButton)
+        val discoButton = findViewById<Button>(R.id.discoButton)
 
         var pressedOnce1 = false;
-        val handler = Handler();
 
         //click listeners to change Database value of normal led switches
         ledImageButton1.setOnClickListener(){
-            //make an Alert Dialog that prompts the user to "wait" 3 seconds in order to compensate for the required time the RPI takes to get the request
-//            val alert = waitAlertDialogCreate("Wait for it...");
             val drawable = ledImageButton1.drawable as TransitionDrawable
 
             if(!pressedOnce1)
             {
                 myDbReference.child("RaspberryPi/LED/LED1").setValue(1)
-                drawable.startTransition(400);
-//                val led1Runnable = Runnable {
-//                    run()
-//                    {
-//                        if(alert.isShowing){
-//                            alert.dismiss();
-//                            drawable.startTransition(400);
-//                        }
-//                    }
-//                }
-//                alert.setOnDismissListener(){
-//                    handler.removeCallbacks(led1Runnable);
-//                }
-//                handler.postDelayed(led1Runnable, 1500);
+                drawable.startTransition(800);
                 pressedOnce1 = true;
             }
             else
             {
                 myDbReference.child("RaspberryPi/LED/LED1").setValue(0)
-                drawable.reverseTransition(400);
-//                val openR = Runnable {
-//                    run()
-//                    {
-//                        if(alert.isShowing){
-//                            alert.dismiss();
-//                            drawable.reverseTransition(400);
-//                        }
-//                    }
-//                }
-//                alert.setOnDismissListener(){
-//                    handler.removeCallbacks(openR);
-//                }
-//                handler.postDelayed(openR, 1500);
+                drawable.reverseTransition(800);
                 pressedOnce1 = false;
             }
         }
 
         var pressedOnce2 = false;
         ledImageButton2.setOnClickListener(){
-//            val alert = waitAlertDialogCreate("Wait for it...");
             val drawable = ledImageButton2.drawable as TransitionDrawable
             if(!pressedOnce2)
             {
                 myDbReference.child("RaspberryPi/LED/LED2").setValue(1)
-                drawable.startTransition(400);
-//                val led2Runnable = Runnable {
-//                    run()
-//                    {
-//                        if(alert.isShowing){
-//                            alert.dismiss();
-//                            drawable.startTransition(400);
-//                        }
-//                    }
-//                }
-//                alert.setOnDismissListener(){
-//                    handler.removeCallbacks(led2Runnable);
-//                }
-//                handler.postDelayed(led2Runnable, 1500);
+                drawable.startTransition(800);
                 pressedOnce2 = true;
             }
             else
             {
                 myDbReference.child("RaspberryPi/LED/LED2").setValue(0)
-                drawable.reverseTransition(400);
-//                val led2Runnable = Runnable {
-//                    run()
-//                    {
-//                        if(alert.isShowing){
-//                            alert.dismiss();
-//                            drawable.reverseTransition(400);
-//                        }
-//                    }
-//                }
-//                alert.setOnDismissListener(){
-//                    handler.removeCallbacks(led2Runnable);
-//                }
-//                handler.postDelayed(led2Runnable, 1500);
+                drawable.reverseTransition(800);
                 pressedOnce2 = false;
             }
         }
 
-
-
-        //click listeners to change Database value of rgb led switches
-        rgbLedRedSwitch.setOnClickListener(){
-            if(rgbLedRedSwitch.isChecked)
-                myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(1)
-            else
-                myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        //color buttons for the RGB LED
+        redButton.setOnClickListener(){
+            redSet();
         }
-        rgbLedGreenSwitch.setOnClickListener(){
-            if(rgbLedGreenSwitch.isChecked)
-                myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(1)
-            else
-                myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        greenButton.setOnClickListener(){
+            greenSet();
         }
-        rgbLedBlueSwitch.setOnClickListener(){
-            if(rgbLedBlueSwitch.isChecked)
-                myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(1)
-            else
-                myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        blueButton.setOnClickListener(){
+            blueSet();
+        }
+        magentaButton.setOnClickListener(){
+            magentaSet()
+        }
+        cyanButton.setOnClickListener(){
+            cyanSet()
+        }
+        yellowButton.setOnClickListener(){
+            yellowSet()
+        }
+        whiteButton.setOnClickListener(){
+            whiteSet()
+        }
+        offButton.setOnClickListener(){
+            offSet()
+        }
+        discoButton.setOnClickListener(){
+            discoSet()
         }
 
         val returnButton = findViewById<Button>(R.id.returnButtonLED)
@@ -154,16 +113,77 @@ class LEDActivity : AppCompatActivity(){
             finish();
         }
     }
+    private fun redSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
 
-    private fun waitAlertDialogCreate(message: String): AlertDialog {
-        val builder = AlertDialog.Builder(this)
-        builder.setMessage(message)
-        val dialog = builder.show()
-        val messageText = dialog.findViewById<View>(android.R.id.message) as TextView?
-        messageText!!.gravity = Gravity.CENTER
-        messageText.textSize = 20F
-        dialog.show()
-        dialog.window?.setLayout(580,270)
-        return dialog;
     }
+    private fun greenSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun blueSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun magentaSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun cyanSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun yellowSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun whiteSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(1)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun offSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(0)
+
+    }
+    private fun discoSet()
+    {
+        myDbReference.child("RaspberryPi/LED/rgbLED/Red").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Green").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Blue").setValue(0)
+        myDbReference.child("RaspberryPi/LED/rgbLED/Disco").setValue(1)
+    }
+
+
 }
